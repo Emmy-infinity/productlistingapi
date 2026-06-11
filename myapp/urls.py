0e15f976d3bbd04e2fd38f6977dd9d3477c57761
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-06-11 03:23:56 +0300

    Remove image upload/list endpoints (requires Pillow)

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,22 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    
-    
-   
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="note-delete"),
-    path('sensor_reading/' ,views.ChartDataView.as_view()),
-    path('upload/', views.ImageUploadView.as_view(), name='image-upload'),
-    path('images/', views.ImageListView.as_view(), name='image-list'),
-    
-  
-
+    path('sensor_reading/', views.ChartDataView.as_view()),
 ]
-
-
-
-
-
-
-

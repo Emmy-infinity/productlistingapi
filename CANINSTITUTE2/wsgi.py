7f20aa1960b7cc4526099cc800ac0bc0aab70aca
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-06-12 15:38:13 +0300

    Update wsgi.py
    an update to meet render requirement

--- CANINSTITUTE2/wsgi.py
+++ CANINSTITUTE2/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CANINSTITUTE2.settings')
+# Explicitly assign the default settings module string to os.environ.
+# This prevents potential process-wide collision errors on modern WSGI app daemons.
+os.environ['DJANGO_SETTINGS_MODULE'] = 'CANINSTITUTE2.settings'
 
 application = get_wsgi_application()
+
